90e250d175b04c0672b085991c67c5c7c26c67cc
Brandon Hays <brandon.hays@gmail.com>
2015-04-02 16:54:21 -0500

    add isDigit helper

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,8 @@
 import CardParser from './utils/card-parser';
 import CardInputParser from './mixins/card-input-parser';
 
+export function isDigit(event) {
+  return String.fromCharCode(event.charCode).match(/\d/);
+}
+
 export { CardParser, CardInputParser };
